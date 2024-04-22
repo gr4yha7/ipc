7d4219aa4d626b6cb8ccbf39ede2d725cccd71a2
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2024-04-23 00:19:36 +0100

    feat: add game modes, switch to pleco searcher

--- fendermint/vm/chess_engine/src/engine.rs
+++ fendermint/vm/chess_engine/src/engine.rs
@@ -1,27 +1,25 @@
 use std::{borrow::BorrowMut, error::Error};
-
-// use pleco::bots::ParallelMiniMaxSearcher;
 use pleco_engine::{
-  engine::PlecoSearcher,threadpool::threadpool, time::uci_timer::PreLimits
+  engine::PlecoSearcher, time::uci_timer::PreLimits
 };
-use pleco::{tools::pleco_arc::Arc, BitMove, Board};
+use pleco::Board;
 
-
-// Get fen string from player (subnet)
-// Construct board from fen string (Board::from_fen())
-// Get AI move (await_move()) --- get best move from pleco searcher
-// Check if AI move is valid ---> if board.generateMoves() contains AI move
-// Apply move
-// Get fen string from board (board.fen()) ---> send to subnet (frontend)
-
+const END_GAME: &str = "endgame";
 pub struct ChessEngine {
-  searcher: Arc<PlecoSearcher>
+  searcher: PlecoSearcher,
+  game_mode: GameMode
+}
+
+enum GameMode {
+  Easy,
+  Hard
 }
 
 impl ChessEngine {
   fn new() -> Self {
     Self {
-      searcher: Arc::new(PlecoSearcher::init(false))
+      searcher: PlecoSearcher::init(false),
+      game_mode: GameMode::Easy
     }
   }
 
@@ -30,32 +28,52 @@ impl ChessEngine {
     board.fen()
   }
 
-  fn play_move(&self, fen_str: &str) -> Result<String, Box<dyn Error>> {
-    let mut current_board = Board::from_fen(fen_str).unwrap();
+  fn play_move(&mut self, fen_str: String) -> Result<String, Box<dyn Error>> {
+    let mut current_board = Board::from_fen(fen_str.as_str()).unwrap();
     if current_board.checkmate() {
-      // todo! end game
+      // current_board.turn()
+      return Ok(END_GAME.to_string());
     }
-    // todo!(
-    //   // set appropriate limits for easy and hard game modes
-    // );
-    let mut prelimits = PreLimits::blank();
-    prelimits.depth = Some(10);
+    
+    // Set limits for easy and hard game modes
+    let mut limits = PreLimits::blank();
+    match self.game_mode {
+        GameMode::Easy => {
+          limits.depth = Some(10)
+        },
+        GameMode::Hard => {
+          limits.mate = Some(5) // checkmate in five moves
+        }
+    }
 
-    let limits = PreLimits::create(prelimits);
+    self.searcher.search(&current_board, &limits);
+    let engine_move = self.searcher.await_move();
 
-    // let engine_move = self.searcher.await_move();
-    let engine_move = threadpool().search(&current_board, &limits);
+
+    // let engine_move = threadpool().search(&current_board, &limits);
     let new_board: String;
-    if is_valid_move(current_board.borrow_mut(), engine_move.stringify().as_str()) {
+    if is_valid_move(current_board.borrow_mut(), engine_move.to_string().as_str()) {
       current_board.apply_move(engine_move);
       if current_board.checkmate() {
-        // todo! end game
+        return Ok(END_GAME.to_string());
       }
       new_board = current_board.fen();
       Ok(new_board)
     } else {
       Err("invalid move".into())
     }
+  }
+
+  pub fn get_move(&mut self, fen: String, depth: u16) -> String {
+    let mut limit = PreLimits::blank();
+    limit.depth = Some(depth);
+    let board = Board::from_fen(fen.as_str()).unwrap();
+    // let mut s = PlecoSearcher::init(false);
+
+    self.searcher.search(&board, &limit);
+    let bit_move = self.searcher.await_move();
+
+    bit_move.to_string()
   }
 }
 
@@ -80,7 +98,7 @@ fn is_valid_move(board: &mut Board, mov: &str) -> bool {
 #[cfg(test)]
 mod tests {
     use pleco::board::fen::OPENING_POS_FEN;
-    use fendermint_vm_actor_interface::eam::EthAddress;
+    // use fendermint_vm_actor_interface::eam::EthAddress;
 
     use super::*;
 
@@ -89,9 +107,18 @@ mod tests {
     fn test_setup_chess_board() {
         let engine = init();
         assert_eq!(engine.start_game(), OPENING_POS_FEN);
-        let addr = EthAddress::from_id(49);
-        println!("actor addr = {:?}", addr);
-        // assert_eq!([0u8; 20], addr.to_vec());
+        // let addr = EthAddress::from_id(49); // 0xff00000000000000000000000000000000000031
+        // println!("actor addr = {:?}", addr);
+    }
+
+    #[test]
+    fn it_works() {
+      let mut engine = init();
+      let result = engine.get_move(
+          "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string(),
+          10,
+      );
+      assert_eq!(result, "e2e4");
     }
 }
 
